b0565b3477820d7f9594172eb0f46976271ba7d6
TJ-Hooker15 <37968340+TJ-Hooker15@users.noreply.github.com>
2021-04-05 02:46:39 +0000

    Update void_luks_setup.bash

--- void_luks_setup.bash
+++ void_luks_setup.bash
@@ -4,8 +4,8 @@ efi_part_size="260M"		#Minimum of 100M, Arch wiki recommends at least 260M (as o
 root_part_size="20G"		#Size of the root partition. Required size depends on how much software you ultimately install, but Arch wiki recommends 15-20G (as of 24-Mar-2021)
 swap_size="4G"			#If you want to use suspend-to-disk (AKA hibernate), should be >= amount of RAM.
 				#Otherwise, equal to square root of RAM (rounded up), or at least 2G
-username=""			#Desired username for regular (non-root) user of the Void installation you're making
-hostname="voidLinux"		#Desired name to be used for the hostname of the Void installation as well as the volume group name
+username="user"			#Desired username for regular (non-root) user of the Void installation you're making
+hostname="desktop"		#Desired name to be used for the hostname of the Void installation as well as the volume group name
 fs_type="ext4"			#Desired filesystem to be used for the root and home partitions
 libc="musl" 			#"musl" for musl, "" for glibc
 language="en_US.UTF-8"
@@ -14,22 +14,59 @@ vendor_gpu="amd"		#Enter either "amd", "intel", or "nvidia" (all lowercase)
 				#For AMD will install the OpenGL and Vulkan driver (mesa, not amdvlk), as well as the video acceration drivers. Does not install the Xorg drivers, you must install the separately if you want to use Xorg
 				#For Intel this installs OpenGL and Vulkan drivers, and video acceleration drivers
 				#For Nvidia this installs the proprietary driver. It assumes you're using a non-legacy GPU, which generally means any Geforce 600 or newer GTX card (some of the low end GT cards from 600, 700, and 800 series are legacy) 
-graphical_de="xfce4"		#Either "xfce4" for the standard XFCE install that you would get if you install using the XFCE live image
-				#Or "kde" for a 'minimal' KDE Plasma install with Wayland
-apps="nano flatpak elogind dbus alsa apparmor ufw cron"
-#apps for intel CPU: nonfree-repo, intel-ucode
-#apps for amd CPU: linux-firmware-amd
-#apps for amd GPU: linux-firmware-amd mesa-dri vulkan-loader mesa-vulkan-radeon mesa-vaapi mesa-vdpau xf86-video-amdgpu
-#apps for intel GPU: linux-firmware-intel mesa-dri mesa-vulkan-intel intel-video-accel xf86-video-intel
-#apps for nvidia GPU: nonfree-repo nvidia
-#apps for kde: emptty plasma-workspace konsole alsa kcron pulseaduio plasma-pa plasma-firewall dolphin #plasma-nm NetworkManager
-#apps for xfce: xorg-minimal xorg-fonts xterm lightdm lightdm-gtk3-greeter xfce4
+graphical_de="xfce"		#"xfce" for the standard XFCE install that you would get if you install using the XFCE live image
+                        	#Or "kde" for a 'minimal' KDE Plasma install with Wayland
+                        	#Leave black (just double quotes, "") to not install DE. Will skip graphics driver installation as well
+
+apps="nano flatpak elogind dbus alsa-utils apparmor ufw cronie ntp"
 rm_services=("agetty-tty2" "agetty-tty3" "agetty-tty4" "agetty-tty5" "agetty-tty6" "mdadm" "sshd" "acpid" "NetworkManager")
-en_services=("dbus" "elogind" "dhcpcd" "emptty" "ufw" "cron" "ntpd")
+en_services=("dbus" "elogind" "dhcpcd" "emptty" "ufw" "cronie" "ntpd")
+user_groups="wheel" #floppy,cdrom,optical,audio,video,kvm,xbuilder
 
 
-declare luks_pw root_pw user_pw
-declare -a disk_array
+declare apps_intel_cpu="intel-ucode"
+declare apps_amd_cpu="linux-firmware-amd"
+declare apps_amd_gpu="linux-firmware-amd mesa-dri vulkan-loader mesa-vulkan-radeon mesa-vaapi mesa-vdpau xf86-video-amdgpu"
+declare apps_intel_gpu="linux-firmware-intel mesa-dri mesa-vulkan-intel intel-video-accel xf86-video-intel"
+declare apps_nvidia_gpu="nvidia"
+declare apps_kde="emptty plasma-workspace konsole kcron pulseaudio ark plasma-pa plasma-firewall dolphin" #plasma-nm NetworkManager
+declare apps_xfce="xorg-minimal xorg-fonts xterm lightdm lightdm-gtk3-greeter xfce4"
+declare luks_pw root_pw user_pw disk_selected
+
+
+case $vendor_cpu in
+    "amd")
+        apps="$apps apps_amd_cpu"
+        ;;
+    "intel")
+        apps="$apps apps_intel_cpu"
+        ;;
+esac
+
+if [[ -n $graphical_de ]]
+then
+    case $vendor_gpu in
+        "amd")
+            apps="$apps apps_amd_gpu"
+            ;;
+        "intel")
+            apps="$apps apps_intel_gpu"
+            ;;
+        "nvidia")
+            apps="$apps apps_nvidia_gpu"
+            ;;
+    esac
+fi
+
+case $graphical_de in
+    "kde")
+        apps="$apps apps_kde"
+        ;;
+    "xfce")
+        apps="$apps apps_xfce"
+        ;;
+esac
+
 
 echo -e "\nEnter password to be used for disk encryption\n"
 read luks_pw
@@ -40,19 +77,13 @@ read root_pw
 echo -e "\nEnter password to be used for the user account\n"
 read user_pw
 
-while IFS= read -r line; do
-	if [ "$line" != "" ]
-	then
-		disk_array+=("$(echo $line | sed 's/:.*//')")
-		printf '%s %s\n' "${#disk_array[*]}" "$line"
-	fi
-done < <(fdisk -l | grep -v mapper | grep -o '/.*GiB')
-
-printf "\nSelect disk to be installed to, by entering the number to the left of the desired device and hitting Enter\n"
-
-read selection
-
-disk_selected=${disk_array[((selection-1))]}
+PS3="Select disk for installation: "
+select line in $(fdisk -l | grep -v mapper | grep -o '/.*GiB' | tr -d ' ')
+do
+        echo "Selected disk: $line"
+        disk_selected=$(echo $line | sed 's/:.*$//')
+        break
+done
 
 
 if [[ $(echo $disk_selected | grep -o sd) == "sd" ]]
@@ -69,34 +100,29 @@ fi
 
 wipefs -aq $disk_selected
 
-printf 'label: gpt\n, %s, U, *\n, , L\n' "$efi_part_size" | sfdisk "$disk_selected" #-q
+printf 'label: gpt\n, %s, U, *\n, , L\n' "$efi_part_size" | sfdisk -q "$disk_selected"
 
-echo $luks_pw | cryptsetup -q luksFormat --type luks1 $luks_part #-q
+echo $luks_pw | cryptsetup -q luksFormat --type luks1 $luks_part
 
-echo $luks_pw | cryptsetup luksOpen $luks_part $hostname #-q
+echo $luks_pw | cryptsetup -q luksOpen $luks_part $hostname
 
-echo -e "\npress enter\n"
-read tmp
 
-vgcreate $hostname /dev/mapper/$hostname #-q
+vgcreate -q $hostname /dev/mapper/$hostname
 
-lvcreate --name root -L $root_part_size $hostname #-q
-lvcreate --name swap -L $swap_size $hostname
-lvcreate --name home -l 100%FREE $hostname
+lvcreate -q --name root -L $root_part_size $hostname
+lvcreate -q --name swap -L $swap_size $hostname
+lvcreate -q --name home -l 100%FREE $hostname
 
-mkfs.$fs_type -L root /dev/$hostname/root #-q
-mkfs.$fs_type -L home /dev/$hostname/home
+mkfs.$fs_type -qL root /dev/$hostname/root
+mkfs.$fs_type -qL home /dev/$hostname/home
 mkswap /dev/$hostname/swap
-
-echo -e "\npress enter\n"
-read tmp
 
 mount /dev/$hostname/root /mnt
 for dir in dev proc sys run; do mkdir -p /mnt/$dir ; mount --rbind /$dir /mnt/$dir ; mount --make-rslave /mnt/$dir ; done
 mkdir -p /mnt/home
 mount /dev/$hostname/home /mnt/home
 
-mkfs.vfat $efi_part #-q
+mkfs.vfat -q $efi_part
 mkdir -p /mnt/boot/efi
 mount $efi_part /mnt/boot/efi
 
@@ -108,37 +134,24 @@ read tmp
 luks_uuid=$(blkid -o value -s UUID $luks_part)
 
 
-sudo cp /etc/resolv.conf /mnt/etc/
+cp /etc/resolv.conf /mnt/etc/
+
+echo "Press any key\n"
+read tmp
 
 
+chroot /mnt chown root:root /
+chroot /mnt chmod 755 /
 
-echo '#!/bin/bash' > /mnt/chrootSetup.bash
-echo -e "root_pw=\"$root_pw\"" >> /mnt/chrootSetup.bash
-echo -e "hostname=\"$hostname\"" >> /mnt/chrootSetup.bash
-echo -e "fs_type=\"$fs_type\"" >> /mnt/chrootSetup.bash
-echo -e "language=\"$language\"" >> /mnt/chrootSetup.bash
-echo -e "efi_part=\"$efi_part\"" >> /mnt/chrootSetup.bash
-#echo -e "luks_uuid=\"$luks_UUID\"" >> /mnt/chrootSetup.bash
-echo -e "luks_pw=\"$luks_pw\"" >> /mnt/chrootSetup.bash
-echo -e "luks_part=\"$luks_part\"" >> /mnt/chrootSetup.bash
-echo -e "disk_selected=\"$disk_selected\"" >> /mnt/chrootSetup.bash
-echo -e "username=\"$username\"" >> /mnt/chrootSetup.bash
-echo -e "user_pw=\"$user_pw\"" >> /mnt/chrootSetup.bash
-echo -e "luks_uuid=\"$luks_uuid\"" >> /mnt/chrootSetup.bash
-
-#echo '
-chown root:root /mnt/
-chmod 755 /mnt/
-echo -e "$root_pw\n$root_pw" | passwd -q root
 echo $hostname > /mnt/etc/hostname
 echo "LANG=$language" > /mnt/etc/locale.conf
-# echo "en_US.UTF-8 UTF-8" >> /mnt/etc/default/libc-locales
-# xbps-reconfigure -r /mnt/ -f glibc-locales
 
-#luks_uuid=$(blkid -o value -s UUID $luks_part)
+if [[ -z $libc ]]
+then
+    echo "en_US.UTF-8 UTF-8" >> /mnt/etc/default/libc-locales
+    xbps-reconfigure -r /mnt/ -f glibc-locales
+fi
 
-#echo -e "# <file system>	<dir>	<type>	<options>	<dump>	<pass>" > /mnt/etc/fstab
-#echo -e "tmpfs	/tmp	tmpfs	defaults,nosuid,nodev	0	0" >> /etc/fstab
 echo -e "/dev/$hostname/root	/	$fs_type	defaults	0	0" >> /mnt/etc/fstab
 echo -e "/dev/$hostname/home	/home	$fs_type	defaults	0	0" >> /mnt/etc/fstab
 echo -e "/dev/$hostname/swap	swap	swap	defaults	0	0" >> /mnt/etc/fstab
@@ -151,10 +164,16 @@ sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"rd.lvm.vg=$
 
 dd bs=1 count=64 if=/dev/urandom of=/mnt/boot/volume.key
 
-echo $luks_pw | cryptsetup -q luksAddKey $luks_part /boot/volume.key #-q
+cat << EOF | chroot /mnt
+echo "$root_pw\n$root_pw" | passwd -q root
+echo $luks_pw | cryptsetup -q luksAddKey $luks_part /boot/volume.key
+useradd $username
+usermod -aG wheel $username
+echo "$user_pw\n$user_pw" | passwd -q $username
+EOF
 
-chmod 000 /mnt/boot/volume.key
-chmod -R g-rwx,o-rwx /mnt/boot
+chroot /mnt chmod 000 /boot/volume.key
+chroot /mnt chmod -R g-rwx,o-rwx /boot
 
 echo "$hostname	$luks_part	/boot/volume.key	luks" >> /mnt/etc/crypttab
 
@@ -162,34 +181,41 @@ echo -e "install_items+=\" /boot/volume.key /etc/crypttab \"" > /mnt/etc/dracut.
 
 
 
-grub-install $disk_selected
+chroot /mnt grub-install $disk_selected
 
 
 xbps-reconfigure -r /mnt/ -fa
-useradd $username
-usermod -aG wheel $username
-echo -e "$user_pw\n$user_pw" | passwd $username
-sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /mnt/etc/sudoers
-#' >> /mnt/chrootSetup.bash
 
-#Edit emptty config file
-tty=2
-sudo sed -i "s/^#*TTY_NUMBER=[0-9]*/TTY_NUMBER=$tty/i" /mnt/etc/emptty/conf
-sudo sed -i "s/^#*DEFAULT_USER=/DEFAULT_USER=$user_name/i" /mnt/etc/emptty/conf
+sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /mnt/etc/sudoers
+
+xbps-install -Suyr /mnt xbps
+
+if [[ $gpu == "nvidia" ]] || [[ $cpu == "intel" ]]
+then
+    xbps-install -Syr /mnt/ void-repo-nonfree
+fi
+
+xbps-install -Syr /mnt $apps
   
 #Disable services
 for service in ${rm_services[@]}
 do
-	sudo rm /var/service/$service
+	sudo rm /etc/runit/runsvdir/default/$service
 done
   
 #Enable services
 for service in ${en_services[@]}
 do
-	sudo ln -s /etc/sv/$service /var/service/
+	sudo ln -s /etc/sv/$service /etc/runit/runsvdir/default/
 done 
 
 sed -i 's/^#*APPARMOR=.*$/APPARMOR=complain/i' /mnt/etc/default/apparmor
+
+
+#Edit emptty config file
+tty=2
+sed -i "s/^#*TTY_NUMBER=[0-9]*/TTY_NUMBER=$tty/i" /mnt/etc/emptty/conf
+sed -i "s/^#*DEFAULT_USER=/DEFAULT_USER=$user_name/i" /mnt/etc/emptty/conf
 
 #Enable SSD trim
 
